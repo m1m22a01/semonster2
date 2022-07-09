0a57bd775b970e07f6893bdba6966df444ad525b
m1m22a18 <m1m22a18@oit.ac.jp>
2022-07-09 18:00:59 +0900

    add game

--- app/src/main/java/semonster2/SeMonsterGame.java
+++ app/src/main/java/semonster2/SeMonsterGame.java
@@ -3,39 +3,51 @@ package semonster2;
 import java.util.Scanner;
 
 public class SeMonsterGame {
-  Player player;
-  Monster my_monster;
+  // 初期設定
   Monster monster;
+  Monster my_monster;
+  Player player;
   Scanner scanner = new Scanner(System.in);
 
-  public void addPlayer(String name) {
-    player = new Player(name);
+  // 名前追加メソッド
+  public void addPlayer(String playerName) {
+    // Scanner scan = new Scanner(System.in); //
+    Player add_player = new Player(playerName);
+    this.player = add_player;
   }
 
-  public void draw(String name) {
-    player.showDeck();
+  // 自身のモンスターdrawメソッド
+  public void draw(String playerName) {
+    this.player.showDeck();
   }
 
   public void battle(Monster monster) {
-    player.showDeck();
     this.monster = monster;
-    int num = scanner.nextInt();
-    my_monster = player.deck.get(num);
-    while (true) {
-      System.out.printf("--Summon Monster %s--\n", my_monster.name);
-      this.monster.HP -= my_monster.attack;
-      System.out.printf("my monster attack %d\n", my_monster.attack);
-      my_monster.HP -= this.monster.attack;
-      System.out.printf("monster attack %d\n", my_monster.attack);
+    int num;
 
+    System.out.println("\n----------------------------------------------");
+    // 以下対戦処理
+    System.out.println(this.monster.showMonster());
+    // 敵のHPがゼロになるまで繰り返す
+    while (true) {
+      this.player.showDeck();
+      System.out.println("\n--Summon Monster in deck(1-5)--");
+      num = scanner.nextInt();
+      my_monster = player.deck.get(num);
+      this.monster.HP -= my_monster.attack;
+      System.out.printf("%dで攻撃 %dのダメージ\n", my_monster.attack, this.monster.HP);
+      my_monster.HP -= this.monster.attack;
+      System.out.printf("%sに%dのダメージ\n", my_monster.name, this.monster.HP);
+
+      // 自分の手持ちがのHPがゼロになったら負け
       if (my_monster.HP <= 0) {
-        System.out.println("lose");
+        System.out.println("負け");
         break;
       } else if (monster.HP <= 0) {
-        System.out.println("win");
+        System.out.println("勝ち");
         player.levelUp();
-        break;
       }
+
     }
   }
 }
